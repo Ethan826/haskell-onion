70784e2f4d9905f785206da99745020d6f0e9957
Ethan Kent <ekent@jd11.law.harvard.edu>
2024-04-30 21:17:46 -0400

    refactored v1

--- app/providers/PostgresUserRepositoryV1.hs
+++ app/providers/PostgresUserRepositoryV1.hs
@@ -27,10 +27,32 @@ import Data.Vector (Vector, fromList)
 import Database.PostgreSQL.Simple (
   Connection,
   FromRow,
+  Query,
+  ToRow,
   query,
  )
 import GHC.Generics (Generic)
 import Services.UserRepository (UserRepository (..))
+
+------------------------------------------------------------
+-- Main export
+------------------------------------------------------------
+
+instance UserRepository PostgresUserRepositoryV1 where
+  getUserById :: UserId -> PostgresUserRepositoryV1 (Maybe User)
+  getUserById (HumanId userId) = do
+    humans <- runQuery userByIdQuery [unId userId]
+    pure $ HumanUser . humanFromDbHuman <$> listToMaybe humans
+  getUserById (OrganizationId userId) = do
+    dbResult <- runQuery organizationByIdQuery [unId userId]
+    pure $ (listToMaybe . (organizationFromDb <$>)) dbResult
+
+  getAllHumansInOrganization ::
+    OrganizationId ->
+    PostgresUserRepositoryV1 (Vector Human)
+  getAllHumansInOrganization organizationId = do
+    humans <- runQuery humansInOrganizationQuery [unId organizationId]
+    pure $ fromList $ humanFromDbHuman <$> humans
 
 newtype PostgresUserRepositoryV1Env = PostgresUserRepositoryV1Env
   { postgresConnection :: Connection
@@ -50,7 +72,42 @@ newtype PostgresUserRepositoryV1 a = PostgresUserRepositoryV1
     , MonadIO
     )
 
-data DbHuman = DbHuman {dbHumanId :: Int, dbHumanName :: String}
+------------------------------------------------------------
+-- SQL Queries
+------------------------------------------------------------
+
+userByIdQuery :: Query
+userByIdQuery =
+  "SELECT id, name FROM humans WHERE id = (?)"
+
+organizationByIdQuery :: Query
+organizationByIdQuery =
+  [r|
+SELECT o.id AS organization_id
+        , o.name AS organization_name
+        , ARRAY_AGG(oh.human_id) AS human_ids
+FROM organizations o
+INNER JOIN organization_humans oh ON o.id = oh.organization_id
+WHERE o.id = (?)
+GROUP BY o.id; |]
+
+humansInOrganizationQuery :: Query
+humansInOrganizationQuery =
+  [r|
+SELECT h.*
+FROM organizations o
+INNER JOIN organization_humans oh ON o.id = oh.organization_id
+INNER JOIN humans h ON h.id = oh.human_id
+WHERE o.id = (?); |]
+
+------------------------------------------------------------
+-- DB -> Domain conversions
+------------------------------------------------------------
+
+data DbHuman = DbHuman
+  { dbHumanId :: Int
+  , dbHumanName :: String
+  }
   deriving (Generic, Show, Eq)
 
 instance FromRow DbHuman
@@ -62,64 +119,28 @@ humanFromDbHuman DbHuman{..} =
     , humanName = pack dbHumanName
     }
 
-data DbOrganization = DbOrganization {dbOrganizationId :: Int, dbOrganizationName :: String}
+data DbOrganization = DbOrganization
+  { dbOrganizationId :: Int
+  , dbOrganizationName :: String
+  }
   deriving (Generic, Show, Eq)
 
 instance FromRow DbOrganization
 
-instance UserRepository PostgresUserRepositoryV1 where
-  getUserById :: UserId -> PostgresUserRepositoryV1 (Maybe User)
-  getUserById (HumanId userId) = do
-    conn <- asks postgresConnection
-    humans <- liftIO $ query conn queryString [unId userId]
-    pure $ HumanUser . humanFromDbHuman <$> listToMaybe humans
-   where
-    queryString = "SELECT * FROM humans WHERE id = (?)"
-  getUserById (OrganizationId userId) = do
-    conn <- asks postgresConnection
-    dbResult <- liftIO $ query conn queryString [unId userId]
-    pure $ (listToMaybe . (tupleToOrganization <$>)) dbResult
-   where
-    tupleToOrganization :: (Int, Text, Vector Int) -> User
-    tupleToOrganization
-      ( organizationId
-        , organizationName
-        , organizationHumanIds
-        ) =
-        OrganizationUser $
-          Organization
-            { organizationId = Id organizationId
-            , organizationName
-            , organizationHumanIds = Id <$> organizationHumanIds
-            }
-    queryString =
-      [r|
-        SELECT o.id AS organization_id
-               , o.name AS organization_name
-               , ARRAY_AGG(oh.human_id) AS human_ids
-        FROM organizations o
-        INNER JOIN organization_humans oh
-        ON o.id = oh.organization_id
-        WHERE o.id = (?)
-        GROUP BY o.id
-      |]
+organizationFromDb :: (Int, Text, Vector Int) -> User
+organizationFromDb (orgId, orgName, orgHumanIds) =
+  OrganizationUser $
+    Organization
+      { organizationId = Id orgId
+      , organizationName = orgName
+      , organizationHumanIds = Id <$> orgHumanIds
+      }
 
-  getAllHumansInOrganization ::
-    OrganizationId ->
-    PostgresUserRepositoryV1 (Vector Human)
-  getAllHumansInOrganization organizationId = do
-    conn <- asks postgresConnection
-    humans :: [DbHuman] <- liftIO $ query conn queryString [unId organizationId]
-    pure $ fromList $ humanFromDbHuman <$> humans
-   where
-    queryString =
-      [r|
-        SELECT h.id
-               , h.name
-        FROM organizations o
-        INNER JOIN organization_humans oh
-                ON o.id = oh.organization_id
-        INNER JOIN humans h
-                ON h.id = oh.human_id
-        WHERE o.id = (?);
-      |]
+runQuery ::
+  (ToRow q, FromRow r) =>
+  Query ->
+  q ->
+  PostgresUserRepositoryV1 [r]
+runQuery q params = do
+  conn <- asks postgresConnection
+  liftIO $ query conn q params
